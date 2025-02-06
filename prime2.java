24a7035657d865e19fffa99f87f851c43c6773a5
AnandGupta-01 <anandgupta05677@gmail.com>
2025-02-06 23:05:15 +0530

    first commit

---
+++ prime2.java
@@ -0,0 +1,14 @@
+
+public class PrimeOrNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int n=24;
+     if(n%2 == 0) {
+    	 System.out.println("it is even no:"+ n);
+     }else {
+     System.out.println("it is odd no:"+ n);
+     
+	}
+	}
+	}
